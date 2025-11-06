faefef574189153214cd47b208f9827592839cb9
pranshu7489768236 <pranshumca@gmail.com>
2025-11-06 11:21:29 +0530

    Squashed 'frontend/' changes from 489320a..6edcb05
    6edcb05 fix: remove duplicate services/guards/components causing NG0908, disable interceptor temporarily
    
    git-subtree-dir: frontend
    git-subtree-split: 6edcb052b158ad4c1dc9bd2ecf80746f49c182f2

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
-import { jwtInterceptor } from './app/core/jwt.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
-    provideHttpClient(withInterceptors([jwtInterceptor]))
+    provideHttpClient()
   ]
 }).catch(err => console.error(err));
