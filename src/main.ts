4bc96e27d1c9253903c93b410171472950659bc3
pranshu7489768236 <pranshumca@gmail.com>
2025-11-06 11:17:14 +0530

    Squashed 'frontend/' changes from 8aab1b9..489320a
    489320a fix: downgrade Angular to 17.0.0 stable and re-enable HTTP interceptor
    
    git-subtree-dir: frontend
    git-subtree-split: 489320a76e8b7ab0832c64feced42188cce47550

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
-// import { jwtInterceptor } from './app/core/jwt.interceptor';
+import { jwtInterceptor } from './app/core/jwt.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([jwtInterceptor]))
   ]
 }).catch(err => console.error(err));
